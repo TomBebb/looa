121290e77db4ccfbb2eb9ee90022a20bf043134a
Tom Bebbington <tombebb@protonmail.com>
2018-09-18 09:44:10 +0000

    Implement fmt::Display for Value

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 #[macro_use]
 extern crate nom;
 
-use std::fmt;
-use std::marker::PhantomData;
-use std::rc::Rc;
-use std::collections::HashMap;
-use std::ops::Drop;
 use std::any::Any;
-use std::cmp::{PartialOrd, PartialEq, Ordering};
+use std::cmp::{Ordering, PartialEq, PartialOrd};
+use std::fmt;
 use std::mem;
+use std::ops::Drop;
+use std::rc::Rc;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum Type {
@@ -19,25 +17,24 @@ pub enum Type {
     Function,
     Userdata,
     Thread,
-    Table
+    Table,
 }
 
 pub trait ConvertValue: Sized {
     const TYPE: Type;
     fn into_value(self) -> Value {
         let data = InnerValueData {
-            ty: Self::TYPE,
-            val: self
+            _ty: Self::TYPE,
+            val: self,
         };
         Value {
-            data: unsafe {
-                mem::transmute(Rc::new(data))
-            }
+            data: unsafe { mem::transmute(Rc::new(data)) },
         }
     }
     unsafe fn from_value_raw(val: &Value) -> &Self {
         let data: &ValueData = &*val.data;
-        let data: &InnerValueData<Self> = mem::transmute(data);
+        let data: &InnerValueData<Self> =
+            &*(data as *const ValueData as *const InnerValueData<Self>);
         &data.val
     }
     fn from_value(val: &Value) -> Option<&Self> {
@@ -67,37 +64,49 @@ impl ConvertValue for Box<Any> {
 }
 
 struct ValueData {
-    ty: Type
+    ty: Type,
 }
 
 struct InnerValueData<T> {
-    ty: Type,
-    val: T
+    _ty: Type,
+    val: T,
 }
 
 #[derive(Clone)]
 pub struct Value {
-    data: Rc<ValueData>
+    data: Rc<ValueData>,
 }
 impl Value {
     pub fn nil() -> Value {
         Value::new(())
     }
-    pub fn new<T>(val: T) -> Value where T: ConvertValue {
+    pub fn new<T>(val: T) -> Value
+    where
+        T: ConvertValue,
+    {
         ConvertValue::into_value(val)
     }
     pub fn type_of(&self) -> Type {
         self.data.ty
     }
     unsafe fn drop<T>(&mut self) {
-        let mut other = Rc::new(ValueData {
-            ty: Type::Nil
-        });
+        let mut other = Rc::new(ValueData { ty: Type::Nil });
         mem::swap(&mut self.data, &mut other);
         let _: Rc<InnerValueData<T>> = mem::transmute(other);
     }
 }
-
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        unsafe {
+            match self.type_of() {
+                Type::Nil => f.write_str("nil"),
+                Type::Boolean => fmt::Display::fmt(bool::from_value_raw(self), f),
+                Type::Number => fmt::Display::fmt(f32::from_value_raw(self), f),
+                _ => unimplemented!(),
+            }
+        }
+    }
+}
 impl PartialEq for Value {
     fn eq(&self, other: &Value) -> bool {
         let (ty, other_ty) = (self.type_of(), other.type_of());
@@ -109,7 +118,7 @@ impl PartialEq for Value {
                     Type::Nil => true,
                     Type::Boolean => bool::from_value_raw(self) == bool::from_value_raw(other),
                     Type::Number => f32::from_value_raw(self) == f32::from_value_raw(other),
-                    _ => unimplemented!()
+                    _ => unimplemented!(),
                 }
             }
         }
@@ -123,12 +132,18 @@ impl PartialOrd for Value {
             None
         } else {
             unsafe {
-                    match ty {
-                        Type::Nil => Some(Ordering::Equal),
-                        Type::Boolean => PartialOrd::partial_cmp(bool::from_value_raw(self), bool::from_value_raw(other)),
-                        Type::Number => PartialOrd::partial_cmp(f32::from_value_raw(self), f32::from_value_raw(other)),
-                        _ => unimplemented!()
-                    }
+                match ty {
+                    Type::Nil => Some(Ordering::Equal),
+                    Type::Boolean => PartialOrd::partial_cmp(
+                        bool::from_value_raw(self),
+                        bool::from_value_raw(other),
+                    ),
+                    Type::Number => PartialOrd::partial_cmp(
+                        f32::from_value_raw(self),
+                        f32::from_value_raw(other),
+                    ),
+                    _ => unimplemented!(),
+                }
             }
         }
     }
@@ -141,7 +156,7 @@ impl Drop for Value {
                 Type::Boolean => Value::drop::<bool>(self),
                 Type::Number => Value::drop::<f32>(self),
                 Type::String => Value::drop::<String>(self),
-                _ => unimplemented!()
+                _ => unimplemented!(),
             }
         }
     }
@@ -149,5 +164,5 @@ impl Drop for Value {
 
 fn main() {
     let val = Value::new(false);
-    println!("Hello, world!");
+    println!("{}", val);
 }
